a6bbce6970c71279c6f3a784c33e3b6afe9bbfaa
Warren Crago <warren.crago45@gmail.com>
2020-09-18 00:53:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,21 @@
-def countdown_with_sleep(number)
+def countdown(number)
   number = 10
   while number <= 10 
   puts "#{number} SECOND(S)!"
   number -= 1
   break if number == 0
+end
+return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+  number = 10
+  while number <= 10 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep 1 
+  break if number == 0 
 end
 return "HAPPY NEW YEAR!"
 end
